cb6569777c297794aa92941076a9829f163d972e
Nadifa Tri Nuraida <a710230010@student.ums.ac.id>
2024-06-11 08:26:00 +0700

    day 9.py

---
+++ day 9.py
@@ -0,0 +1,9 @@
+# Daftar angka yang ingin diurutkan
+angka = [5, 2, 9, 1, 14, 6]
+
+# Menggunakan metode sorted untuk mengurutkan daftar angka
+angka_terurut = sorted(angka)
+
+# Menampilkan hasil
+print("Daftar angka sebelum diurutkan:", angka)
+print("Daftar angka setelah diurutkan:", angka_terurut)
