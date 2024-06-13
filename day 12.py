c7547db384944035c78775511087d38a428b913e
Nadifa Tri Nuraida <a710230010@student.ums.ac.id>
2024-06-14 00:02:20 +0700

    day 12.py

---
+++ day 12.py
@@ -0,0 +1,8 @@
+# Daftar angka
+angka = [10, 20, 30, 40, 50]
+
+# Menghitung rata-rata
+rata_rata = sum(angka) / len(angka)
+
+# Menampilkan hasil
+print("Rata-rata dari daftar angka adalah:", rata_rata)
