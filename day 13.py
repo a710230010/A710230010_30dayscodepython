078d31acf9f6964f43131da2658b3e450e35e564
Nadifa Tri Nuraida <a710230010@student.ums.ac.id>
2024-06-15 08:14:51 +0700

    day 13.py

---
+++ day 13.py
@@ -0,0 +1,8 @@
+# Suhu dalam Celsius
+suhu_celsius = 25
+
+# Mengonversi ke Fahrenheit
+suhu_fahrenheit = (suhu_celsius * 9/5) + 32
+
+# Menampilkan hasil
+print(f"{suhu_celsius} derajat Celsius sama dengan {suhu_fahrenheit} derajat Fahrenheit")
