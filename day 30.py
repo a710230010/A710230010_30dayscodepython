2faa920fd6a0fe09332d9a80bd35eeb46ab8ba9b
Nadifa Tri Nuraida <a710230010@student.ums.ac.id>
2024-07-02 08:25:48 +0700

    day 30.py

---
+++ day 30.py
@@ -0,0 +1,20 @@
+class Mobil:
+    def move(self):
+        print("Berjalan di jalan raya")
+
+class Pesawat:
+    def move(self):
+        print("Terbang di udara")
+
+class Kapal:
+    def move(self):
+        print("Berlayar di laut")
+
+# Contoh penggunaan:
+mobil = Mobil()
+pesawat = Pesawat()
+kapal = Kapal()
+
+mobil.move()    
+pesawat.move()  
+kapal.move()    
